91cdcb07c44119f04ce1a48a928a9c7c4241bcfd
marcore <marco.re@polimi.it>
2014-01-21 18:31:58 +0100

    Aggiunto script per usare stile del tema

---
+++ update.sh
@@ -0,0 +1,12 @@
+## Copy static sass file needed before rake gather asset 
+SASS_DIR="/edx/app/edxapp/edx-platform/lms/static/sass"
+
+cmd="sudo -u edxapp cp ./static/sass/css/* SASS_DIR/ "
+echo $cmd
+$cmd
+
+echo "Compile assets manually"
+sudo -u edxapp bash
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
+rake lms:gather_assets:aws
